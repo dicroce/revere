a8fccdc7e10611b94b52d32f6b1b5b9267ccee4a
Tony Di Croce <dicroce@gmail.com>
2020-12-03 14:41:01 -0800

    added error checking

--- r_utils/source/r_sha_256.cpp
+++ r_utils/source/r_sha_256.cpp
@@ -17,7 +17,8 @@ r_sha_256::r_sha_256() :
 r_sha_256::~r_sha_256() noexcept
 {
     if(!_finalized)
-        SHA256_Final(_digest, &_sha256);
+        if(SHA256_Final(_digest, &_sha256) != 1)
+            R_STHROW(r_internal_exception, ("r_sha_256 SHA256_Final() returned an error."));
 }
 
 void r_sha_256::update( const uint8_t* src, size_t size )
@@ -25,7 +26,8 @@ void r_sha_256::update( const uint8_t* src, size_t size )
     if(_finalized)
         R_STHROW(r_internal_exception, ("r_sha_256 does not support update() called after finalize."));
 
-    SHA256_Update(&_sha256, src, size);
+    if(SHA256_Update(&_sha256, src, size) != 1)
+        R_STHROW(r_internal_exception, ("r_sha_256 SHA256_Update() returned an error."));
 }
 
 void r_sha_256::finalize()
@@ -34,7 +36,8 @@ void r_sha_256::finalize()
     {
         _finalized = true;
 
-        SHA256_Final(_digest, &_sha256);
+        if(SHA256_Final(_digest, &_sha256) != 1)
+            R_STHROW(r_internal_exception, ("r_sha_256 SHA256_Final() returned an error."));
     }
 }
 
@@ -51,13 +54,14 @@ string r_sha_256::get_as_string()
     if( !_finalized )
         R_STHROW(r_internal_exception, ("Please finalize() your r_sha_256 before calling a get() method."));
 
-    return r_string_utils::format( "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-                             _digest[0], _digest[1],_digest[2], _digest[3],
-                             _digest[4], _digest[5], _digest[6], _digest[7],
-                             _digest[8], _digest[9], _digest[10], _digest[11],
-                             _digest[12], _digest[13], _digest[14], _digest[15],
-                             _digest[16], _digest[17],_digest[18], _digest[19],
-                             _digest[20], _digest[21], _digest[22], _digest[23],
-                             _digest[24], _digest[25], _digest[26], _digest[27],
-                             _digest[28], _digest[29], _digest[30], _digest[31] );
+    return r_string_utils::format(
+        "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+        _digest[0], _digest[1],_digest[2], _digest[3],
+        _digest[4], _digest[5], _digest[6], _digest[7],
+        _digest[8], _digest[9], _digest[10], _digest[11],
+        _digest[12], _digest[13], _digest[14], _digest[15],
+        _digest[16], _digest[17],_digest[18], _digest[19],
+        _digest[20], _digest[21], _digest[22], _digest[23],
+        _digest[24], _digest[25], _digest[26], _digest[27],
+        _digest[28], _digest[29], _digest[30], _digest[31]);
 }
